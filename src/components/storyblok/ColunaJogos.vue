c8f0229dd87754fff102bfa4db72cd68e32b0612
Gabriel Cordeiro <cordeiro.faria97@gmail.com>
2022-06-26 11:12:31 -0300

    adicionando sistema de pagination a colunaJogos

--- src/components/storyblok/ColunaJogos.vue
+++ src/components/storyblok/ColunaJogos.vue
@@ -1,16 +1,51 @@
 <template>
-    <b-col cols="3">
+    <b-col>
         <div class="col-direita">
             <h1>{{ blok.titulo }}</h1>
-            <div v-for="blok in blok.jogos" :key="blok._uid">
-                <StoryblokComponent :blok="blok" />
+            <div v-for="jogo in currentJogos" :key="jogo._uid">
+                <StoryblokComponent :blok="jogo" />
             </div>
+            <b-pagination
+                v-model="currentPage"
+                :total-rows="totalJogos"
+                :per-page="perPage"
+                aria-controls="my-list"
+            ></b-pagination>
         </div>
     </b-col>
 </template>
 
-<script setup>
-defineProps({ blok: Object })
+<script>
+export default {
+    props: {
+        blok: {
+            type: Object,
+            default: () => {}
+        }
+    },
+    data() {
+        return {
+            perPage: 3,
+            currentPage: 1,
+            currentJogos: [],
+        }
+    },
+    computed: {
+        totalJogos() {
+            return this.blok.jogos.length
+        },
+    },
+    mounted() {
+        this.currentJogos = this.blok.jogos.slice(0, this.perPage)
+    },
+    watch: {
+        currentPage(paginaAtual) {
+            const inicio = this.perPage * paginaAtual - this.perPage
+            const fim = this.perPage * paginaAtual
+            this.currentJogos = this.blok.jogos.slice(inicio, fim)
+        },
+    },
+}
 </script>
 
 <style>
@@ -18,7 +53,7 @@ defineProps({ blok: Object })
         display: flex;
         flex-direction: column;
         align-items: center;
-        height: 100vh;
+        height: auto;
         background-color: darkred;
         color: #FFF;
         width: 100%;
