491eb663e00245a69189fd9840b142c003b28357
Gabriel Cordeiro <cordeiro.faria97@gmail.com>
2025-03-20 21:43:33 -0300

    corrigindo imagem header

--- storyblok/Header.vue
+++ storyblok/Header.vue
@@ -1,22 +1,25 @@
 <template>
 <div class="header__principal">
     <b-col fluid cols="2" class="header__logo">
-        <router-link to="/"> <img src="../../assets/flamenguismoLogo.png" alt="EscudoDoFlamengo" class="img-logo"> </router-link>
+        <NuxtLink to="/"> <img :src="header.imagem.filename" alt="EscudoDoFlamengo" class="img-logo"> </NuxtLink>
     </b-col>
     <b-col class="header__menu">
-        <router-link to="/" class="linksPrincipais">Home</router-link>
-        <router-link to="/noticias" class="linksPrincipais">Notícias</router-link>
-        <router-link to="/historia" class="linksPrincipais">História</router-link>
-        <router-link to="/equipe" class="linksPrincipais">Equipe</router-link>
-        <router-link to="/jogos" class="linksPrincipais">Jogos</router-link>
+        <NuxtLink to="/" class="linksPrincipais">Home</NuxtLink>
+        <NuxtLink to="/noticias" class="linksPrincipais">Notícias</NuxtLink>
+        <NuxtLink to="/historia" class="linksPrincipais">História</NuxtLink>
+        <NuxtLink to="/equipe" class="linksPrincipais">Equipe</NuxtLink>
+        <NuxtLink to="/jogos" class="linksPrincipais">Jogos</NuxtLink>
     </b-col>
 </div>
 </template>
 
-<script lang="ts">
-    export default {
-        name: 'HeaderMain'
-    }
+<script setup>
+const story = await useAsyncStoryblok('config', {
+  version: 'draft',
+})
+const header = computed(() => {
+  return story.value.content.header[0]
+})
 </script>
 
 <style>
